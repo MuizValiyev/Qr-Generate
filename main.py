a4bc7842e6c559c5346393f3a6406876d676f4e8
Muiz <141940146+MuizValiyev@users.noreply.github.com>
2024-03-28 14:15:34 +0500

    qwerty

---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request
+import segno
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/generate', methods=['POST'])
+def generate_qr():
+    data = request.form['data']
+    qr = segno.make(data, micro=False)
+    return render_template('qrcode.html', qrcode=qr)
+
+if __name__ == '__main__':
+    app.run(debug=True)
